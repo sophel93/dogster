955cd8cc039ad5f3f684ddc2c185aa9d4ba81f69
sophel93 <elliottsophia2@gmail.com>
2022-02-13 13:32:05 +0200

    Show all profiles on front page if user is logged in

--- index.php
+++ index.php
@@ -17,10 +17,28 @@ require 'header.php';
 
 
 <section class="index-content">
+    <?php if (isset($_SESSION['id'])) { 
+
+        require 'includes\dbhandler.php';
+
+        $sql = "SELECT username, location FROM signup_info";
+        $result = mysqli_query($connect, $sql);
+        
+        if ($result->num_rows > 0 ) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo $row['username'], $row['location'];
+                
+            }
+        mysqli_close($connect);
+        }
+    } else { ?>
     <h1>Lorem ipsum</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim dignissim quam sed maximus. 
         Nunc iaculis sapien sit amet sem iaculis, eget sodales leo varius. Vestibulum a imperdiet ipsum, 
         vitae ullamcorper ipsum. Maecenas eget sem ligula. Donec sit amet aliquam libero. Curabitur eget ipsum pulvinar,</p>
+    <?php } ?>
 </section>
+
+ 
 
 <?php require 'footer.php'; 
